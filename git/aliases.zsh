0f7fb3e1e00d6389c88b599209bf79f91ab8f9b2
dosire <sytses@gmail.com>
2014-02-26 11:16:09 +0100

    Add push for website deploy.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,3 +19,4 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 alias gpa="git push origin $1 && git push gh $1 && git push gl $1" # Push GitLab to GitLab dev server, GitHub and GitLab Cloud
+alias gpd="git push deploy master && git push origin master"
