8987ab7da547288d6b7dcce07abdf0dfd5d7d379
Sytse Sijbrandij <sytses@gmail.com>
2014-09-24 13:33:43 +0200

    Fix rake tasks such as bundle exec rake new_post["Feature highlight: Groups"]

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,7 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Allow [ or ] whereever you want for rake tasks
+unsetopt nomatch
+
