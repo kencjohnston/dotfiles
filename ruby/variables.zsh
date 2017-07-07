7a350570954781afbb96b2ca58086371404208eb
dosire <sytse@gitlab.com>
2017-07-07 13:33:45 -0700

    Fix the warning.

--- ruby/variables.zsh
+++ ruby/variables.zsh
@@ -1,2 +1,4 @@
 unset RUBY_FREE_MIN
-
+# this doesn't work because it is later set again, so we set RUBY_GC_HEAP_FREE_SLOTS instead
+export RUBY_GC_HEAP_FREE_SLOTS=200000
+# that works because of https://bugs.ruby-lang.org/issues/9276
